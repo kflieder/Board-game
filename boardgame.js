749027f3d67949767b8f43de6eaf93acc0cc5789
kflieder <125841074+kflieder@users.noreply.github.com>
2023-03-28 21:14:52 -0600

    added some button functionality
    click 'draw tile' then click an empty space on the board and repeat :D

--- boardgame.js
+++ boardgame.js
@@ -1,3 +1,46 @@
 
+// Add event listeners to each tile to detect when the player clicks on it.
+// Add event listeners to each square to detect when the player clicks on it.
+// Inside the event listener for the tile, store a reference to the selected tile.
+// Inside the event listener for the square, check if a tile has been selected and then place the selected tile on the square.
+
+
+
+
+let board = document.getElementById('board-el')
+
+const spaces = document.querySelectorAll('.space')
+const tileWithPic = document.getElementById('.with-pic')
+const tileWithPath = document.getElementById('.tile') 
+let gameStarted = false
+
+const allPathTiles = ['straight', 'four-way path', 'turn left']
+const allPicTiles = []
+
+
+
+spaces.forEach((space, index) => {
+	
+
+	space.addEventListener('click', () => {
+		space.innerText = drawnTile.innerText	})
+})
+
+
+const drawTileBtn = document.getElementById('draw-tile-btn')
+let drawnTile = document.getElementById('drawn-tile')
+
+
+
+drawTileBtn.addEventListener('click', () => {
+	const randomIndex = Math.floor(Math.random() * allPathTiles.length);
+	drawnTile.innerText = allPathTiles[randomIndex]
+})
+
+
+
+
+
+
 
 
