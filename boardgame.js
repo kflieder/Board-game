8221ee9e19d4a60e61518f7f9ebb2b3e8eb8fbef
kflieder <125841074+kflieder@users.noreply.github.com>
2023-03-30 21:07:28 -0600

    character
    there's now a moveable character, but only after a tile is drawn and placed on the board.

--- boardgame.js
+++ boardgame.js
@@ -1,12 +1,3 @@
-
-// Add event listeners to each tile to detect when the player clicks on it.
-// Add event listeners to each square to detect when the player clicks on it.
-// Inside the event listener for the tile, store a reference to the selected tile.
-// Inside the event listener for the square, check if a tile has been selected and then place the selected tile on the square.
-
-
-
-
 let board = document.getElementById('.board-el')
 
 const spaces = document.querySelectorAll('.space')
@@ -17,37 +8,50 @@ let gameStarted = false
 const allPathTiles = ['straight', 'four-way path', 'turn left']
 const allPicTiles = []
 
-
-
 tileWithPath.forEach((currentTile) => {
-		currentTile.addEventListener('click', () => {
-		addTileToBoard(currentTile, drawnTile);
-			})
+  currentTile.addEventListener('click', () => {
+    addTileToBoard(currentTile, drawnTile);
+  })
 })
 
 
 const drawTileBtn = document.getElementById('draw-tile-btn')
 let drawnTile = document.getElementById('drawn-tile')
 
-
-
 drawTileBtn.addEventListener('click', () => {
-	const randomIndex = Math.floor(Math.random() * allPathTiles.length);
-	drawnTile.innerText = allPathTiles[randomIndex]
+  const randomIndex = Math.floor(Math.random() * allPathTiles.length);
+  drawnTile.innerText = allPathTiles[randomIndex]
 })
-
 
 function addTileToBoard(currentTile, drawnTile) {
   if (currentTile.innerText === '') {
     currentTile.innerText = drawnTile.innerText;
     drawnTile.textContent = '';
+    isCharacterMove = true
   }
 }
 
-
 const playerChar1 = document.createElement('div')
 playerChar1.textContent = "CHARACTER"
-document.getElementById('space1').appendChild(playerChar1)
+document.getElementById('space2').appendChild(playerChar1)
+
+let isPlayerTurn = false
+let isCharacterMove = false
+let isCharacterSelected = false
+
+playerChar1.addEventListener('click', () => {
+  if (isCharacterMove) {
+    isCharacterSelected = true
+  }
+})
+
+spaces.forEach((space) => {
+  space.addEventListener('click', () => {
+    if (isCharacterSelected) {
+      space.appendChild(playerChar1)
+    }
+  })
+})
 
 
 
